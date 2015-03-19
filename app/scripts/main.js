0a35b67428324ca7f66ed8c759677324c322c5ab
Omar Aguirre <omersguchigu@gmail.com>
2015-03-18 22:14:51 -0600

    make setupo for materialize styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,16 +3,34 @@
 
 require.config({
     shim: {
+        materialize:{
+            deps: ['jquery', 'hammerjs', 'velocity']
+        },
+        app:{
+            deps:['materialize']
+        }
     },
     paths: {
         jquery: '../bower_components/jquery/dist/jquery',
         backbone: '../bower_components/backbone/backbone',
-        underscore: '../bower_components/lodash/dist/lodash'
+        'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
+        'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
+        underscore: '../bower_components/lodash/dist/lodash',
+        marionette: '../bower_components/marionette/lib/core/backbone.marionette',
+        materialize: '../bower_components/materialize/bin/materialize',
+        velocity: '../bower_components/materialize/js/velocity.min',
+        hammerjs: '../bower_components/materialize/js/hammer.min'
     }
 });
+require(['app'],function (app){
+    app.start();
+});
 
+
+/*
 require([
     'backbone'
-], function (Backbone) {
+], function (Backbone,jQuery) {
     Backbone.history.start();
 });
+*/
