33a2ab93932367254445fb679d6c7ab22c18c3ff
Omar Aguirre <omersguchigu@gmail.com>
2015-03-20 17:33:22 -0600

    update config marionette

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,32 @@
 define([
-    'backbone',
-    'marionette'
-], function (Backbone,Marionette) {
-    app = new Marionette.Application();
-    app.on('start',function(){
-        console.log('jala');
-       if (Backbone.history) {
-           Backbone.history.start();
-       }
+    'marionette',
+    'backbone'
+], function (Marionette, Backbone) {
+    var app = new Marionette.Application();
+
+    app.addRegions({
+        headerRegion: 'header',
+        mainRegion: 'main',
+        footerRegion: 'footer'
+    });
+
+    app.navigate = function(route, options){
+        options || (options = {});
+        Backbone.history.navigate(route, options);
+    };
+
+    app.getCurrentRoute = function(){
+        return Backbone.history.fragment
+    };
+
+    app.on('start', function () {
+        console.log('start');
+        if (Backbone.history) {
+            Backbone.history.start();
+        }
+        /*if(this.getCurrentRoute() === ""){
+            app.trigger("generate:module");
+        }*/
     });
 
     return app;
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,36 +1,32 @@
 /*global require*/
-'use strict';
 
 require.config({
     shim: {
         materialize:{
-            deps: ['jquery', 'hammerjs', 'velocity']
+            deps: ['jquery']
         },
-        app:{
-            deps:['materialize']
+        jquery: {
+            exports: '$'
         }
     },
     paths: {
-        jquery: '../bower_components/jquery/dist/jquery',
+        jquery: '../bower_components/jquery/dist/jquery.min',
         backbone: '../bower_components/backbone/backbone',
         'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
         'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
         underscore: '../bower_components/lodash/dist/lodash',
         marionette: '../bower_components/marionette/lib/core/backbone.marionette',
-        materialize: '../bower_components/materialize/bin/materialize',
-        velocity: '../bower_components/materialize/js/velocity.min',
-        hammerjs: '../bower_components/materialize/js/hammer.min'
+        materialize: '../bower_components/materialize/bin/materialize'
     }
 });
-require(['app'],function (app){
-    app.start();
-});
-
-
-/*
 require([
-    'backbone'
-], function (Backbone,jQuery) {
-    Backbone.history.start();
+    'app',
+    'jquery',
+    'materialize'
+],function (App, $){
+    $(function() {
+        $('select').material_select();
+        $(".button-collapse").sideNav();
+    });
+    App.start();
 });
-*/
