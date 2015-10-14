5114e21c5923326e357f97febb78fc486ff8ae4f
Antonio Antillon <antillas21@gmail.com>
2015-10-14 01:26:16 -0700

    Adds material-ui

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Home from './components/home';
 
+let injectTapEventPlugin = require("react-tap-event-plugin");
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
 window.React = React;
 const mountNode = document.getElementById('app');
 
