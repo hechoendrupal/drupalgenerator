24369b10d4ebea4006b7b65678b849c7bf4a6bff
Antonio Antillon <antillas21@gmail.com>
2015-10-28 03:00:33 -0700

    Adds base GeneratorForm component
    A GeneratorForm component is a smart component. It receives a config
    object and builds itself (a form) from there.

--- app/scripts/components/home.js
+++ app/scripts/components/home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import AppDescription from './app-description';
-import GenerateModuleForm from './generate-module-form';
+import GeneratorForm from './generator-form';
 import AppBar from 'material-ui/lib/app-bar';
 import Tabs from 'material-ui/lib/tabs/tabs';
 import Tab from 'material-ui/lib/tabs/tab';
+import ModuleGeneratorConfig from '../fixtures/module-generator-config.js';
 
 export default class extends React.Component {
   render() {
@@ -13,10 +14,9 @@ export default class extends React.Component {
         <AppDescription />
         <Tabs>
           <Tab label="Generate Module" >
-            <GenerateModuleForm />
+            <GeneratorForm config={ModuleGeneratorConfig()} />
           </Tab>
           <Tab label="Generate Plugin" >
-            (Tab content to generate plugin coming soon...)
           </Tab>
           <Tab label="Generate Command" >
             (Tab content to generate command coming soon...)
